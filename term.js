fa555d02dd658a3cc4c59f5044440b402b447341
Echo-Nexus <vxattitude@gmail.com>
2024-08-05 16:09:43 +0545

    Added teams and conditions files

---
+++ term.js
@@ -0,0 +1,5 @@
+document.getElementById('agree-btn').addEventListener('click', () => {
+    alert('You have agreed to the Terms and Conditions.');
+    // Redirect or perform other actions as needed
+    window.location.href = 'index.html'; // Replace with your desired action
+});
